58eca8d4173e44ad6f08d18c4b18b10bb149db57
P S Mithul Sourav <padmamithul123@gmail.com>
2022-12-11 12:12:25 +0000

    few updates

--- version-2/components/Hero.tsx
+++ version-2/components/Hero.tsx
@@ -3,18 +3,9 @@ function Hero(){
 return (
   
     <div className="snap-y snap-mandatory h-screen w-screen overflow-auto ">
-      <div className="snap-start h-screen w-screen flex items-      center justify-center text-5xl ">
-          <div className='grid gap-2 grid-row-3 content-                    center'>
-            <div><h1 className='text-white row-start-1 '>
-              <span>P S Mithul</span> 
-              <br></br><span>Sourav</span>
-              <br></br></h1>
-            </div>
-            <img src=''></img>
-          </div>
-          
-    
-    </div>
+      <div className="snap-start h-screen w-screen flex items-center justify-center text-5xl text-red-700">
+          1
+      </div>
       <div className="snap-start h-screen w-screen flex items-        center justify-center text-5xl text-red-700">
         2
       </div>
