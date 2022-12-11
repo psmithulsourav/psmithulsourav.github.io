eeaf4992256b6b6f728597224ba190083e2e2030
P S Mithul Sourav <padmamithul123@gmail.com>
2022-12-11 11:30:21 +0000

    added pics

--- version-2/components/Hero.tsx
+++ version-2/components/Hero.tsx
@@ -1,12 +1,28 @@
+import { motion } from 'framer-motion';
 function Hero(){
 return (
   
     <div className="snap-y snap-mandatory h-screen w-screen overflow-auto ">
-    <div className="snap-start h-screen w-screen flex items-center justify-center text-5xl text-red-700">1</div>
-      <div className="snap-start h-screen w-screen flex items-center justify-center text-5xl text-red-700">2</div>
-      <div className="snap-start  h-screen w-screen flex items-center justify-center text-5xl text-red-700">3</div>
+      <div className="snap-start h-screen w-screen flex items-      center justify-center text-5xl ">
+          <div className='grid gap-2 grid-row-3 content-                    center'>
+            <div><h1 className='text-white row-start-1 '>
+              <span>P S Mithul</span> 
+              <br></br><span>Sourav</span>
+              <br></br></h1>
+            </div>
+            <img src=''></img>
+          </div>
+          
     
     </div>
+      <div className="snap-start h-screen w-screen flex items-        center justify-center text-5xl text-red-700">
+        2
+      </div>
+      <div className="snap-start  h-screen w-screen flex             items-center justify-center text-5xl text-red-700">
+        3
+      </div>
+    
+  </div>
   ) 
 }
 export default Hero
--- version-2/pages/index.tsx
+++ version-2/pages/index.tsx
@@ -13,9 +13,8 @@ const Home: NextPage = () => {
         <link rel="icon" href="https://github.com/psmithulsourav/psmithulsourav.github.io/blob/main/version-2/public/pfp_nobg.svg?raw=true" />
     </Head>
   
-  <div className="bg-cover bg-center bg-[#041c34] overlow-hidden">
+  <div className="bg-cover bg-center bg-[#081c3c] overlow-hidden">
       <Header />
-  <img className="-z-50 bg-fixed bg-cover bg-center"src="https://github.com/psmithulsourav/psmithulsourav.github.io/blob/main/version-2/public/moon.png?raw=true"/>
       <Hero />
   </div>
 </div>
