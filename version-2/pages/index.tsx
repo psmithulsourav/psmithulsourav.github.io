39dd6145e7e02b58d39e5c77b3b65844abeb01c7
P S Mithul Sourav <padmamithul123@gmail.com>
2022-12-11 09:48:40 +0000

    more updates

--- version-2/pages/index.tsx
+++ version-2/pages/index.tsx
@@ -7,17 +7,18 @@ import Hero from "../components/Hero";
 
 const Home: NextPage = () => {
   return (
-    <div className="mx-auto ">
-
-      <Head>
+<div>
+    <Head>
         <title>P S Mithul Sourav</title>
         <link rel="icon" href="https://github.com/psmithulsourav/psmithulsourav.github.io/blob/main/version-2/public/pfp_nobg.svg?raw=true" />
-      </Head>
-<div className="bg-cover bg-center bg-[#041c34] overlow-hidden">
+    </Head>
+  
+  <div className="bg-cover bg-center bg-[#041c34] overlow-hidden">
       <Header />
+  
       <Hero />
+  </div>
 </div>
-    </div>
   )
 }
 
